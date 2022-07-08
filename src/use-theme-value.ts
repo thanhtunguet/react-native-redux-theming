5825252b2512e54362bb82b39a8d80576a017d48
thanhtunguet <thanhtung.uet@gmail.com>
2022-07-08 15:24:19 +0700

    chore: fix useThemeValue

--- src/use-theme-value.ts
+++ src/use-theme-value.ts
@@ -3,12 +3,13 @@ import { themeSelector, themesSelector } from './selectors';
 import React from 'react';
 import type { ThemeStyle } from './types';
 
-export function useThemeValue<T = {}>(key: keyof ThemeStyle & T) {
+export function useThemeValue<T = {}>(key: keyof ThemeStyle | keyof T) {
   const currentTheme = useSelector(themeSelector);
 
   const themeMap = useSelector(themesSelector);
 
   return React.useMemo(() => {
+    // @ts-ignore
     return themeMap[currentTheme][key];
   }, [currentTheme, key, themeMap]);
 }
